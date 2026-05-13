cfc7130ddc5ac45873e6604384d501f4ca4b3441
dellindone <adityav961@gmail.com>
2026-05-13 23:30:02 +0530

    feat: upgrade rate limiting to sliding window counter algorithm

--- shopping_store/app/middlewares/rate_limiting.py
+++ shopping_store/app/middlewares/rate_limiting.py
@@ -1,3 +1,4 @@
+import time 
 from fastapi import Request, Depends
 from app.core.redis import redis_client
 from app.core.exceptions import RateLimitException
@@ -6,12 +7,29 @@ from app.models.user import Users
 
 window = 60
 
-async def rate_limit(limit: int, window: int, identifier: str):
+async def rate_limit_fixed_window(limit: int, window: int, identifier: str):
     key = f"rate_limit:{identifier}"
     count = await redis_client.incr(key)
     if count == 1: await redis_client.expire(key, window)
     if count > limit: raise RateLimitException()
 
+async def rate_limit(limit: int, window: int, indentifier: str):
+    now = time.time()
+    current_window_start = int(now // window) * window
+    previous_window_start = current_window_start - window
+
+    current_key = f"rate_limit:{indentifier}:{current_window_start}"
+    current_count = int(await redis_client.get(current_key) or 0)
+
+    previous_key = f"rate_limit:{indentifier}:{previous_window_start}"
+    previous_count = int(await redis_client.get(previous_key) or 0)
+    
+    time_elapsed = now - current_window_start
+    overlap_ratio = (window - time_elapsed) / window
+    await redis_client.incr(current_key)
+    await redis_client.expire(current_key, window * 2)
+    weighted_count = (previous_count * overlap_ratio) + current_count
+    if weighted_count > limit: raise RateLimitException()
 
 async def register_rate_limit(request: Request):
     await rate_limit(3, window, request.client.host)
